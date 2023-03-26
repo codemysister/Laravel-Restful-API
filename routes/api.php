50cbcfc382aa42865677142133db1af5f7cae289
Deva <theprojessa@gmail.com>
2023-03-27 01:48:15 +0700

    setup searching

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TokenController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('product', ProductController::class);
 Route::post('token-generate', TokenController::class);
+Route::get('search', SearchController::class);
